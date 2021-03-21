608acb37ec6c9e9eea605a22ad140be729bd1a3d
kanli <kang.li@zalando.ie>
2021-03-21 20:59:41 +0000

    init 1

---
+++ src/model/Product.js
@@ -0,0 +1,18 @@
+
+const Product = () => {
+    const id = "id";
+    const name = "name";
+    return (
+        <div>
+            <div id={id}>{id}</div>
+            <div name={name}>{name}</div>
+        </div>
+    )
+
+}
+
+
+
+
+
+export default Product
